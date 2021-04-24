7aada748e9b22d6ff7df3e018f9736a664e7a417
Michał Spyra <>
2021-04-24 12:43:10 +0200

    space-modules

---
+++ space-project-app/src/app/space/space-ship/space-ship.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-space-ship',
+  templateUrl: './space-ship.component.html',
+  styleUrls: ['./space-ship.component.css']
+})
+export class SpaceShipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
